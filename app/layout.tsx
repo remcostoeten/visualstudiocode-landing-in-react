27a207fbff852bc22887319d57c34924c48c8061
Remco Stoeten <remcostoeten@hotmail.com>
2023-07-17 00:50:43 +0200

    cl

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import "@/styles/globals.scss"
 import React from "react"
 import { Metadata } from "next"
-import { ClerkProvider } from "@clerk/nextjs"
+
+// import { ClerkProvider } from "@clerk/nextjs"
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
@@ -34,30 +35,30 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
-      <ClerkProvider>
-        <CursorProvider>
-          <html lang="en">
-            <body
-              className={cn(
-                "bg-background fira-code min-h-screen font-sans antialiased"
-              )}
-            >
-              <Cursor />
-              <ActionsBar />
-              <div className="relative flex min-h-screen bg-body">
-                <div className="flex">
-                  {/* <Sidebar /> */}
-                  <ExploreAside />
-                </div>
-                <div style={{ width: "100%" }}>
-                  <Tabsbar />
-                  <main className="h-[85vh] flex-1 p-2">{children}</main>
-                </div>
+      {/* <ClerkProvider> */}
+      <CursorProvider>
+        <html lang="en">
+          <body
+            className={cn(
+              "bg-background fira-code min-h-screen font-sans antialiased"
+            )}
+          >
+            <Cursor />
+            <ActionsBar />
+            <div className="relative flex min-h-screen bg-body">
+              <div className="flex">
+                {/* <Sidebar /> */}
+                <ExploreAside />
               </div>
-            </body>
-          </html>
-        </CursorProvider>
-      </ClerkProvider>
+              <div style={{ width: "100%" }}>
+                <Tabsbar />
+                <main className="h-[85vh] flex-1 p-2">{children}</main>
+              </div>
+            </div>
+          </body>
+        </html>
+      </CursorProvider>
+      {/* </ClerkProvider> */}
     </>
   )
 }

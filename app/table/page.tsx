3a8fed5d70bd0227406745536d96d166dc91de3d
Remco Stoeten <remcostoeten@hotmail.com>
2023-07-17 21:34:23 +0200

    Deploy

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 // Simulate a database read for tasks.
 async function getTasks() {
   const data = await fs.readFile(
-    path.join(process.cwd(), "app/examples/tasks/data/tasks.json")
+    path.join(process.cwd(), "app/table/data/tasks.json")
   )
 
   const tasks = JSON.parse(data.toString())

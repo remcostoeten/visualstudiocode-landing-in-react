2b35ecd030d94fb5911e11aee9f95998383279c6
Remco Stoeten <remcostoeten@hotmail.com>
2023-07-22 01:26:24 +0200

    Intro

--- app/url/page.jsx
+++ app/url/page.jsx
@@ -18,7 +18,7 @@ export default function MainComponent() {
       <Terminal
         computer="bernard:"
         title="Url extractor & opener tool"
-        paragraph="why dont you extract some urls? :)"
+        paragraph="This is a tool I use quite often and I got absolutely sick off seeing a Cloudfare instance everytime I enter the page followed by a captcha and a bunch of ads. So what do you do then? Right, recreate and host the functionallity yourself 🤢 So that is what I did."
       />
       <Extract inputValue={inputValue} />
     </>

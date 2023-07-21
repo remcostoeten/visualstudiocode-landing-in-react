bb8816a483f2e5fe0f8f5f325f086d098c7de3e1
Remco Stoeten <remcostoeten@hotmail.com>
2023-07-22 01:29:14 +0200

    add clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import "@/styles/globals.scss"
 import React from "react"
 import { Metadata } from "next"
-
-// import { ClerkProvider } from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs"
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
@@ -34,32 +33,32 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
-      {/* <ClerkProvider> */}
-      <CursorProvider>
-        <html lang="en">
-          <body
-            className={cn(
-              "bg-background fira-code min-h-screen font-sans antialiased"
-            )}
-          >
-            <Cursor />
-            <ActionsBar />
-            <div className="relative flex min-h-screen bg-body">
-              <div className="flex">
-                {/* <Sidebar /> */}
-                <ExploreAside />
+      <ClerkProvider>
+        <CursorProvider>
+          <html lang="en">
+            <body
+              className={cn(
+                "bg-background fira-code min-h-screen font-sans antialiased"
+              )}
+            >
+              <Cursor />
+              <ActionsBar />
+              <div className="relative flex min-h-screen bg-body">
+                <div className="flex">
+                  {/* <Sidebar /> */}
+                  <ExploreAside />
+                </div>
+                <div style={{ width: "100%" }}>
+                  <Tabsbar />
+                  <main className="h-[85vh] max-w-max flex-1 p-2">
+                    {children}
+                  </main>
+                </div>
               </div>
-              <div style={{ width: "100%" }}>
-                <Tabsbar />
-                <main className="h-[85vh] max-w-max flex-1 p-2">
-                  {children}
-                </main>
-              </div>
-            </div>
-          </body>
-        </html>
-      </CursorProvider>
-      {/* </ClerkProvider> */}
+            </body>
+          </html>
+        </CursorProvider>
+      </ClerkProvider>
     </>
   )
 }
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,8 +1,6 @@
 import * as React from "react"
 import Link from "next/link"
 
-// import { SignIn, UserButton } from "@clerk/nextjs"
-
 import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
